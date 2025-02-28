03dda14c9903df80bb057a1de688469057be331d
Cipriano <cipriano.guilherem@icloud.com>
2025-02-28 00:42:55 -0300

    First commit.

---
+++ src/test/java/com/cipriano/cipripay/CipripayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cipriano.cipripay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CipripayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
